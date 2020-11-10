1278755d6dcfc1ba2cee71b1fc24855f01f0bbb3
gregster7 <ggoldgof@gmail.com>
2020-11-10 14:14:42 -0800

    created first webpages

--- labeller/urls.py
+++ labeller/urls.py
@@ -1,5 +1,5 @@
-"""HL_site URL Configuration
-
+"""Defines URL patterns for labller"""
+"""
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include, re_path
+
+from django.conf.urls import re_path
+
+from . import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'', include(('labeller.urls', 'labeller'), namespace='labller'))
+	# Home page
+	re_path(r'^$', views.index, name='index')
 ]
--- labeller/views.py
+++ labeller/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	"""The home page for labeller"""
+	return render(request, 'labeller/index.html')
